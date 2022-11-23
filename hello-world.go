afa176e3cc0a51f75baa52779df540affe123107
Shantanu Unde <er.shantanuu@gmail.com>
2022-11-23 15:01:00 +0530

    Hello-world

---
+++ hello-world.go
@@ -0,0 +1,11 @@
+package main
+
+// Every Go programm must be start with main package. This line is first line of code.
+
+import "fmt"
+
+//Importing other packages includes in "double quotes" use import keyword
+
+func main() {
+	fmt.Println("Hello, World!")
+}
